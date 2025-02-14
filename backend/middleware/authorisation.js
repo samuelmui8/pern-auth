6d2fb39fab5412a6186b62d13afc207ceea79815
samuelmui8 <samuelmui8@gmail.com>
2025-02-14 16:13:52 +0800

    Add authorisation and validation middleware

---
+++ backend/middleware/authorisation.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const authenticateToken = (req, res, next) => {
+    const token = req.header("token");
+    if (!token) return res.status(401).json({ message: "Access denied" });
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = payload.user;
+        next();
+    } catch (err) {
+        res.status(403).json({ message: "Invalid token" });
+    }
+};
+
+module.exports = authenticateToken;
---
+++ backend/middleware/validation.js
@@ -0,0 +1,23 @@
+module.exports = (req, res, next) => {
+    const { username, password, nric, first_name, last_name, dob, address, gender } = req.body;
+
+    if (req.path === "/register") {
+        if (![username, password, nric, first_name, last_name, dob, address, gender].every(Boolean)) {
+            return res.status(401).json({ message: "Missing Credentials" });
+        }
+        // check nric validity
+        if (!/^[STFG]\d{7}[A-Z]$/.test(nric)) {
+            return res.status(401).json({ message: "Invalid NRIC" });
+        }
+
+    }
+    
+    if (req.path === "/login") {
+        if (![username, password].every(Boolean)) {
+            return res.status(401).json({ message: "Missing Credentials" });
+        }
+    }
+    
+    next();
+};
+
